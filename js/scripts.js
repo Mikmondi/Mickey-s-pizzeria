3fb8bd15003894e66cf62ec89a96d3c814ec7614
Mikmondi <mikmondi@gmail.com>
2021-04-27 06:18:20 +0300

    added business logic functions to the js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
 //Business Logic
+var small = 650;
+var medium = 850;
+var large = 1200;
+function Pizza(small,medium,regular){
+    this.small = 650;
+    this.medium = 850;
+    this.large = 1200;
+}
+function Toppings(pepperoni,pineapples,cheese){
+    this.pepperoni = 80;
+    this.pineapples = 60;
+    this.cheese = 100;
+}
+function Crust(italianThin,americanHandTossed,panPizza){
+    this.italianThin = 70;
+    this.americanHandTossed = 60;
+    this.panPizza = 50;
+}
